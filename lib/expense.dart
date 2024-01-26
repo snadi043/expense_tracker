3df36a3ad4aeb1f490b10aa6a5d2d1c56607a1e1
snadi043 <snadi043@uottawa.ca>
2024-01-26 09:30:53 -0500

    Basic first layout to produce the output on the screen and created first custom widget to start the application

---
+++ lib/expense.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Expense extends StatefulWidget {
+  const Expense({super.key});
+
+  @override
+  State<Expense> createState() {
+    return _ExpenseState();
+  }
+}
+
+class _ExpenseState extends State<Expense> {
+  @override
+  build(BuildContext context) {
+    return const Text('Hello Expense Tracker!');
+  }
+}
