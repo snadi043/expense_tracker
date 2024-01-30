98745299d38992a9e97eb508bc8f19f24d5d8fda
snadi043 <snadi043@uottawa.ca>
2024-01-30 09:53:15 -0500

    Applying theming concepts to the app using customized app wide theme and configuring the import widgets like Text, Card, Button and AppBar with best suitable feel and look mostly by rewritting the default formats using copyWith() method.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widget/expenses_home_screen.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 98, 60, 180));
 void main() {
   runApp(const MyApp());
 }
@@ -9,8 +11,30 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   build(BuildContext context) {
-    return const MaterialApp(
-      home: Expenses(),
+    return MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+                titleLarge: TextStyle(
+              fontWeight: FontWeight.normal,
+              fontSize: 14,
+              color: kColorScheme.onSecondaryContainer,
+            )),
+      ),
+      home: const Expenses(),
     );
   }
 }
